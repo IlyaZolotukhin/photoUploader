ce60ea5de4690fd9f7d7c27311653c1c038463f7
IlyaZolotukhin <1zolotyhin1@gmail.com>
2024-11-16 15:13:27 +0700

    version 1.7 add addButton

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,21 +1,24 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import type { TuiFileLike } from '@taiga-ui/kit';
+import { Injectable } from '@angular/core';//декоратор
+import { BehaviorSubject } from 'rxjs';// для хранения и передачи текущего значения
+import type { TuiFileLike } from '@taiga-ui/kit';//тип файла используемый в тайге
 
 interface SelectedFile {
   file: TuiFileLike | null;
   textValue: string | null;
 }
-
+//декоратор нужен для внедрения в другие компоненты или сервисы
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'//параметр для доступа во всем приложении
 })
 export class FileService {
+  //свойство selectedFileSubject происходит от BehaviorSubject
   private selectedFileSubject = new BehaviorSubject<SelectedFile | null>(null);
+  //selectedFiles$ поток данных на которые можно подписаться
   selectedFiles$ = this.selectedFileSubject.asObservable();
-
+//в метод setSelectedFile приходят два параметра
   setSelectedFile(file: TuiFileLike | null, textValue: string | null): void {
     const selectedFile: SelectedFile = { file, textValue };
+    //методом next обновляем текущее значение потока
     this.selectedFileSubject.next(selectedFile);
   }
 }
